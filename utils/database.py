ebc60fac2dc43c3b4ccff7e0c9d4154ac958ed28
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2024-12-19 21:53:57 +0300

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -5,11 +5,11 @@ import pandas as pd
 from tabulate import tabulate
 
 def execute_query(query):
-    conn = sqlite3.connect("taif_medical.db")  # Connect to the database file
+    conn = sqlite3.connect("taif_medical.db")
     try:
         # Split multiple SQL statements
         for stmt in query.split(";"):
-            if stmt.strip():  # Execute only if the statement is not empty
+            if stmt.strip():
                 cur = conn.cursor()
                 cur.execute(stmt)
                 results = cur.fetchall()
@@ -17,15 +17,15 @@ def execute_query(query):
                 col_names = [desc[0] for desc in cur.description]
                 # Display results in a table
                 if results:
-                  print(tabulate(results, headers=col_names, tablefmt="fancy_grid"))  # Use col_names here
+                    print(tabulate(results, headers=col_names, tablefmt="fancy_grid"))
                 else:
-                  print("No results found.")
+                    print("No results found.")
     except Exception as e:
         print(f"Error executing SQL query: {e}")
     finally:
         conn.close()
+
 def format_results(results):
-    # ... (Implement your logic to format the results) ...
     # Example:
     if isinstance(results, list):
         formatted_results = ""
