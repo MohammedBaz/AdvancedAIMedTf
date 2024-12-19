263f4906416852fb1ab244670b246ae314182b10
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2024-12-19 13:47:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 # config.py
 
 import streamlit as st
-
+import sqlite3
+import pandas as pd
+import random
+from tabulate import tabulate
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+import google.generativeai as genai
+import streamlit as st
 # Access the API key from Streamlit secrets
 API_KEY = st.secrets["GeminiKey"]
 
