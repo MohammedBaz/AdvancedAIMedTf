770a47cef6073ee484dbcaeef7492b622292f4a9
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2024-12-19 13:55:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
-# config.py
-
 import streamlit as st
-import sqlite3
-import pandas as pd
-import random
-from tabulate import tabulate
-import google.generativeai as genai
-from utils.database import execute_query
-from model import get_gemini_response, extract_sql_query, extract_contextualization
-import config
 
 # Access the API key from Streamlit secrets
 API_KEY = st.secrets["GeminiKey"]
@@ -18,3 +8,7 @@ DATABASE_NAME = "taif_medical.db"
 
 # Gemini model settings
 MODEL_NAME = "gemini-pro"
+
+# Import from the correct location
+from utils.database import execute_query
+from model import get_gemini_response, extract_sql_query, extract_contextualization
