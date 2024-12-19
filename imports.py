ebc802a24723db80456d5bb6239f0ecb563de8ce
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2024-12-19 14:04:07 +0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -6,6 +6,19 @@ import pandas as pd
 import random
 from tabulate import tabulate
 import google.generativeai as genai
+
+# Access the API key from Streamlit secrets
+API_KEY = st.secrets["GeminiKey"]
+
+# Configure Genai Key
+genai.configure(api_key=API_KEY)
+
+# Database settings
+DATABASE_NAME = "taif_medical.db"
+
+# Gemini model settings
+MODEL_NAME = "gemini-pro"
+
+# Import from the correct location
 from utils.database import execute_query
 from model import get_gemini_response, extract_sql_query, extract_contextualization
-import config
